018742d1fa2fcf52abd3b7dec29a8465266b2b82
Corey Richardson <richardsoncorey04@gmail.com>
2025-02-04 18:25:09 +0000

    COMP2005 Demo Springboot Project

---
+++ COMP2005/Workshops/demo-sb/demo-sb/src/main/java/com/example/demosb/MainController.java
@@ -0,0 +1,12 @@
+package com.example.demosb;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @GetMapping("/")
+    public String index() {
+        return "Hello there!!";
+    }
+}
